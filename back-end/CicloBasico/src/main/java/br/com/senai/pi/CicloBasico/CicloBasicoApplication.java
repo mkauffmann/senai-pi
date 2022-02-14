9afada5d3249871d55b2e0b7af85213fd3deee1a
Maria Kauffmann <70024598+mkauffmann@users.noreply.github.com>
2022-02-14 20:38:39 -0300

    projeto inicial spring

---
+++ back-end/CicloBasico/src/main/java/br/com/senai/pi/CicloBasico/CicloBasicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.pi.CicloBasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicloBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicloBasicoApplication.class, args);
+	}
+
+}
---
+++ back-end/CicloBasico/src/test/java/br/com/senai/pi/CicloBasico/CicloBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.pi.CicloBasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicloBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
