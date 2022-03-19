b7aaa07c4f2a2c86b7f720df23d9b7bd1a786d63
Maria Kauffmann <70024598+mkauffmann@users.noreply.github.com>
2022-03-19 13:12:58 -0300

    migracao pagina sobre, mapeamento de endpoints para navegacao

---
+++ back-end/CicloBasico/src/main/java/br/com/senai/pi/CicloBasico/controller/NavController.java
@@ -0,0 +1,34 @@
+package br.com.senai.pi.CicloBasico.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/sobre")
+    public String sobre(){
+        return "sobre";
+    }
+
+    @GetMapping("/catalogo")
+    public String catalogo(){
+        return "catalogo";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/contato")
+    public String contato(){
+        return "contato";
+    }
+
+}
